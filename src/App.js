40b39e546a119fd88af9d1cca17107ee1ca853e1
AbigailNeciosup <85855524+AbigailNeciosup@users.noreply.github.com>
2021-10-29 15:18:00 -0500

    my fist change

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import Dashboard from "./common/dashboard/Dashboard";
 import {BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from "./pages/login/Login";
+import ReactGA from "react-ga";
+import {createBrowserHistory} from 'history'
+
+//Inicializamos React Ga
+    const trackingId = "291726518"
+    ReactGA.initialize(trackingId);
+
+    const history = createBrowserHistory();
+
+    history.listen(location =>{
+      ReactGA.pageview(location.pathname);
+    })
+
+
 function App() {
   return (
     <BrowserRouter>
@@ -12,7 +26,7 @@ function App() {
             <Login></Login>
           </Route>
         {/* Panel de Control (Dashboard) */}
-          <Route path="/principal">
+          <Route path="/principal" history={history}>
             <Dashboard></Dashboard>
           </Route>
       </Switch> 
